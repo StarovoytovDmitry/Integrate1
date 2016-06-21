b730b2a2162af2d55767c81be271d31d0a4a755f
Дмитрий <delfin@MacBook-Air-Dmitrij.local>
2016-06-21 16:45:24 +0300

    gg

--- integrate/test.c
+++ integrate/test.c
@@ -21,5 +21,5 @@ int main(int argc, const char * argv[]) {
     b=integral_monte(f, 1, 5, 100);
     printf("%f %f \n",a,b);
     integral_runge4(df, &x0, &y0, 0.1);
-    printf("x=%f y=%f \n",x0,y0);
+    //printf("x=%f y=%f \n",x0,y0);//izm
 }
