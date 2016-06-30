23ce4f006d7bad6d0ffc0729833f38a70552cd32
Дмитрий <delfin@MacBook-Air-Dmitrij.local>
2016-06-30 16:08:13 +0300

    doxygen ispravlen

--- integrate/html/search/all_1.js
+++ integrate/html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['mnk',['mnk',['../_integrate_8c.html#a1f74296c7602ff7987cccbf998ba44d6',1,'mnk(int s, int n, double *x, double *y, double *a_res[]):&#160;Integrate.c'],['../_integrate_8h.html#a1f74296c7602ff7987cccbf998ba44d6',1,'mnk(int s, int n, double *x, double *y, double *a_res[]):&#160;Integrate.c']]]
+  ['mnk',['mnk',['../_integrate_8c.html#ae7dca3cad1a5b00877f7886bf12af0cf',1,'mnk(int s, int n, double *x, double *y, double *a_res):&#160;Integrate.c'],['../_integrate_8h.html#ae7dca3cad1a5b00877f7886bf12af0cf',1,'mnk(int s, int n, double *x, double *y, double *a_res):&#160;Integrate.c']]]
 ];
